0e65aec14c32fdda80a99b925038917f0609d26a
YOUR_PUBLIC_NAME <YOUR_PUBLIC@GIT_EMAIL.COM>
2017-09-06 22:22:03 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    byte b = 100;
+	    short s = 10000;
+	    int i = 1000000;
+	    long l = 9223372036854775807L;
+	    float f = .0000001f;
+	    double d = 1010.10d;
+	    boolean bl = true;
+	    char c = 'C';
+
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("boolean: " + bl);
+        System.out.println("char: " + c);
+    }
+}
